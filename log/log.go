5159ff173024ca0fb7ea3a54d62b130dc60492e7
Damian Stasiak <stasiakd@ryanair.com>
2019-02-04 08:27:47 +0100

    zap logger

---
+++ log/log.go
@@ -0,0 +1,35 @@
+package log
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/lambdacontext"
+	"go.uber.org/zap"
+)
+
+var log *zap.SugaredLogger
+
+//Creates logger with development config. Logs all from debug level
+func Init(ctx context.Context) {
+	context, _ := lambdacontext.FromContext(ctx)
+	rawLogger, _ := zap.NewDevelopment()
+	defer rawLogger.Sync()
+	log = rawLogger.Sugar().With("context.AwsRequestID", context.AwsRequestID)
+}
+
+func Debug(template string, args ...interface{}) {
+	log.Debugf(template, args)
+}
+
+func Info(template string, args ...interface{}) {
+	log.Infof(template, args)
+}
+
+// Warnf uses fmt.Sprintf to log a templated message.
+func Warn(template string, args ...interface{}) {
+	log.Warnf(template, args)
+}
+
+// Errorf uses fmt.Sprintf to log a templated message.
+func Error(template string, args ...interface{}) {
+	log.Errorf(template, args)
+}
