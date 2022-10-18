9b0fb36f521bacfd766be636279245c3cf307998
sodkiewiczm <sodkiewiczm@ryanair.com>
2022-10-18 16:23:44 +0200

    Setup for dynamo records

--- log/event.go
+++ log/event.go
@@ -7,28 +7,45 @@ import (
 
 func SetUpSns(ctx context.Context, event events.SNSEvent) {
 	SetupTraceIds(ctx)
-	DebugW("Got event",
-		EventSource, "sns",
-		EventBody, ToString(event))
+	if IsDebugEnabled() {
+		DebugW("Got event",
+			EventSource, "sns",
+			EventBody, ToString(event))
+	}
 }
 
 func SetUpSnsRecord(ctx context.Context, event events.SNSEventRecord) {
 	SetupTraceIds(ctx)
-	DebugW("Got event",
-		EventSource, event.EventSource,
-		EventBody, ToString(event))
+	if IsDebugEnabled() {
+		DebugW("Got event",
+			EventSource, event.EventSource,
+			EventBody, ToString(event))
+	}
 }
 
 func SetUpSqs(ctx context.Context, event events.SQSEvent) {
 	SetupTraceIds(ctx)
-	DebugW("Got event",
-		EventSource, "sqs",
-		EventBody, ToString(event))
+	if IsDebugEnabled() {
+		DebugW("Got event",
+			EventSource, "sqs",
+			EventBody, ToString(event))
+	}
 }
 
 func SetUpSqsRecord(ctx context.Context, event events.SQSMessage) {
 	SetupTraceIds(ctx)
-	DebugW("Got event",
-		EventSource, event.EventSource,
-		EventBody, ToString(event))
+	if IsDebugEnabled() {
+		DebugW("Got event",
+			EventSource, event.EventSource,
+			EventBody, ToString(event))
+	}
+}
+
+func SetUpDynamoRecord(ctx context.Context, event events.DynamoDBEventRecord) {
+	SetupTraceIds(ctx)
+	if IsDebugEnabled() {
+		DebugW("Got event",
+			EventSource, event.EventSource,
+			EventBody, ToString(event))
+	}
 }
