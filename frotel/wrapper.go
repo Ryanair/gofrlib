ab08888bbe87eca66fe24bb141efe022ddb5fbc0
sodkiewiczm <sodkiewiczm@ryanair.com>
2023-03-31 19:38:03 +0200

    Add full start handler

--- frotel/wrapper.go
+++ frotel/wrapper.go
@@ -1,14 +1,29 @@
 package frotel
 
 import (
+	"context"
+	"github.com/Ryanair/gofrlib/log"
 	"github.com/aws/aws-lambda-go/lambda"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/trace"
 )
 
-func InstrumentHandler(handlerFunc interface{}) interface{} {
-	return otellambda.InstrumentHandler(handlerFunc)
+func InstrumentHandler(tp *trace.TracerProvider, handlerFunc interface{}) interface{} {
+	return otellambda.InstrumentHandler(handlerFunc,
+		otellambda.WithTracerProvider(tp),
+		otellambda.WithFlusher(tp),
+		otellambda.WithPropagator(propagation.TraceContext{}))
 }
 
 func Start(handlerFunc interface{}) {
-	lambda.Start(InstrumentHandler(handlerFunc))
+	ctx := context.Background()
+	tp, err := NewProvider(ctx)
+	if err != nil {
+		log.Error("creating tracing provider failed", err)
+		tp = trace.NewTracerProvider()
+	}
+	defer tp.Shutdown(ctx)
+
+	lambda.Start(InstrumentHandler(tp, handlerFunc))
 }
