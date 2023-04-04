e63c5ffe27baf621b6080e295e73a13f248166db
sodkiewiczm <sodkiewiczm@ryanair.com>
2023-04-04 15:40:41 +0200

    Add creation of meters provider

---
+++ frotel/traces.go
@@ -0,0 +1,31 @@
+package frotel
+
+import (
+	"context"
+	"github.com/Ryanair/gofrlib/log"
+	"github.com/pkg/errors"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/trace"
+)
+
+func NewTraceProvider(ctx context.Context) (*trace.TracerProvider, error) {
+	exporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithInsecure())
+	if err != nil {
+		log.Error("Error creating exporter: %v", err)
+		return nil, errors.Wrapf(err, "Error creating exporter")
+	}
+
+	resourcesMerged, err := buildResources(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	tp := trace.NewTracerProvider(
+		trace.WithBatcher(exporter),
+		trace.WithResource(resourcesMerged),
+	)
+	otel.SetTracerProvider(tp)
+
+	return tp, nil
+}
