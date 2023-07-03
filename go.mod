6bc884d62a81060b57200306b77c56c5170aed0b
sodkiewiczm <sodkiewiczm@ryanair.com>
2023-07-03 13:23:44 +0200

    Annotate spans with messaging data

--- go.mod
+++ go.mod
@@ -10,9 +10,12 @@ require (
 	github.com/stretchr/testify v1.8.3
 	go.opentelemetry.io/contrib/detectors/aws/lambda v0.42.0
 	go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda v0.42.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.42.0
 	go.opentelemetry.io/otel v1.16.0
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v0.39.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.16.0
 	go.opentelemetry.io/otel/sdk v1.16.0
+	go.opentelemetry.io/otel/sdk/metric v0.39.0
 	go.opentelemetry.io/otel/trace v1.16.0
 	go.uber.org/zap v1.24.0
 )
@@ -35,13 +38,10 @@ require (
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.48.0 // indirect
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.42.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.16.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlpmetric v0.39.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v0.39.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.16.0 // indirect
 	go.opentelemetry.io/otel/metric v1.16.0 // indirect
-	go.opentelemetry.io/otel/sdk/metric v0.39.0 // indirect
 	go.opentelemetry.io/proto/otlp v0.20.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
--- log/event.go
+++ log/event.go
@@ -3,10 +3,20 @@ package log
 import (
 	"context"
 	"github.com/aws/aws-lambda-go/events"
+	semconv "go.opentelemetry.io/otel/semconv/v1.18.0"
+	"go.opentelemetry.io/otel/trace"
 )
+
+//Semantic conventions for messaging: https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/messaging.md
 
 func SetUpSns(ctx context.Context, event events.SNSEvent) {
 	SetupTraceIds(ctx)
+	trace.SpanFromContext(ctx).SetAttributes(
+		semconv.MessagingSystem(MessagingSourceSystemSns),
+		semconv.MessagingBatchMessageCount(len(event.Records)),
+		semconv.MessagingSourceName(event.Records[0].EventSource),
+		semconv.MessagingOperationProcess,
+	)
 	if IsDebugEnabled() {
 		DebugW("Got event",
 			EventSource, "sns",
@@ -16,6 +26,11 @@ func SetUpSns(ctx context.Context, event events.SNSEvent) {
 
 func SetUpSnsRecord(ctx context.Context, event events.SNSEventRecord) {
 	SetupTraceIds(ctx)
+	trace.SpanFromContext(ctx).SetAttributes(
+		semconv.MessagingSystem(MessagingSourceSystemSns),
+		semconv.MessagingSourceName(event.EventSource),
+		semconv.MessagingOperationProcess,
+	)
 	if IsDebugEnabled() {
 		DebugW("Got event",
 			EventSource, event.EventSource,
@@ -25,6 +40,12 @@ func SetUpSnsRecord(ctx context.Context, event events.SNSEventRecord) {
 
 func SetUpSqs(ctx context.Context, event events.SQSEvent) {
 	SetupTraceIds(ctx)
+	trace.SpanFromContext(ctx).SetAttributes(
+		semconv.MessagingSystem(MessagingSourceSystemSqs),
+		semconv.MessagingBatchMessageCount(len(event.Records)),
+		semconv.MessagingSourceName(event.Records[0].EventSource),
+		semconv.MessagingOperationProcess,
+	)
 	if IsDebugEnabled() {
 		DebugW("Got event",
 			EventSource, "sqs",
@@ -34,6 +55,11 @@ func SetUpSqs(ctx context.Context, event events.SQSEvent) {
 
 func SetUpSqsRecord(ctx context.Context, event events.SQSMessage) {
 	SetupTraceIds(ctx)
+	trace.SpanFromContext(ctx).SetAttributes(
+		semconv.MessagingSystem(MessagingSourceSystemSqs),
+		semconv.MessagingSourceName(event.EventSource),
+		semconv.MessagingOperationProcess,
+	)
 	if IsDebugEnabled() {
 		DebugW("Got event",
 			EventSource, event.EventSource,
@@ -41,8 +67,31 @@ func SetUpSqsRecord(ctx context.Context, event events.SQSMessage) {
 	}
 }
 
+func SetUpDynamoEvent(ctx context.Context, event events.DynamoDBEvent) {
+	SetupTraceIds(ctx)
+	trace.SpanFromContext(ctx).SetAttributes(
+		semconv.MessagingSystem(MessagingSourceSystemDynamoDbStreams),
+		semconv.MessagingBatchMessageCount(len(event.Records)),
+		semconv.MessagingSourceName(event.Records[0].EventSource),
+		semconv.MessagingOperationProcess,
+	)
+	if IsDebugEnabled() {
+		DebugW("Got event",
+			EventSource, event.Records[0].EventSource,
+			EventBody, ToString(event))
+	}
+}
+
 func SetUpDynamoRecord(ctx context.Context, event events.DynamoDBEventRecord) {
 	SetupTraceIds(ctx)
+	trace.SpanFromContext(ctx).SetAttributes(
+		semconv.MessagingSystem(MessagingSourceSystemDynamoDbStreams),
+		semconv.MessagingSourceName(event.EventSource),
+		semconv.MessagingMessageID(event.EventID),
+		MessagingSourceSystemDynamoDbStreamsMessageKey.String(ToString(event.Change.Keys)),
+		semconv.MessagingMessagePayloadSizeBytes(int(event.Change.SizeBytes)),
+		semconv.MessagingOperationKey.String(event.EventName),
+	)
 	if IsDebugEnabled() {
 		DebugW("Got event",
 			EventSource, event.EventSource,
