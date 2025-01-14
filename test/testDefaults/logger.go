723ff804b56d04f85ab5df58875b706b8b1582a1
Klag, Bartlomiej <klagb@ryanair.com>
2025-01-14 10:54:41 +0100

    API-6694: tidy

--- test/testDefaults/logger.go
+++ test/testDefaults/logger.go
@@ -1,5 +1,5 @@
 package testDefaults
 
-import "github.com/Ryanair/gofrlib/log"
+import "bitbucket.org/ryanair/gofrlib/log"
 
 var LoggerConfig = log.NewConfiguration("DEBUG", "TEST-APP", "TEST-PROJECT", "TEST-PROJECT-GROUP", "TEST-VERSION", "TEST-PREFIX", "TEST-ENV")
